4b0d8091683c1c0845c59c9a4ab0b796a31e6694
Vaibhav Kumar <53369163+vaibhavkumar1517@users.noreply.github.com>
2021-01-13 01:55:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 class Library:
     def __init__(self,listofbooks):
         self.books = listofbooks
+
     def displayAvalaibleBooks(self):
         print("Books present in the library are: ")
         for book in self.books:
             print(book)
+
     def borrowBook(self,bookname):
         if bookname in self.books:
             print("Yes the "+bookname + " has been issued by you. Please take care of it and return it within 30 days.")
@@ -13,18 +15,25 @@ class Library:
         else:
             print("Sorry the book has already been issued. Please wait until it is returned. Thank You.")
             return False 
+
     def returnbook(self,bookname):
         self.books.append(bookname)
-        print("Thank you for returning the book. We hope you had a great time reading it.")                   
+        print("Thank you for returning the book. We hope you had a great time reading it.")
+
 class Student:
     def requestbook(self):
         self.book = input("Please mention the bookname you want to borrow: ")
         return self.book
+
     def returnbook(self):
         self.book = input("Please mention the bookname you want to return: ")    
         return self.book
+
+
 if __name__ == "__main__" :
-    centralLibrary = Library(["Algorithms","DSA","OS","CN","Cybersecurity"])
+    print("Please Enter a list of books: ")
+    books_ = list(map(str,input().split()))
+    centralLibrary = Library(books_)
     student = Student()
     while True:
         welcmsg = '''||| WELCOME TO THE CENTRAL LIBRARY |||
@@ -33,6 +42,7 @@ if __name__ == "__main__" :
         2. Request a book 
         3. Return a book
         4. Exit the library  '''
+
         print(welcmsg)      
         c = int(input("Enter your choice : "))
         if c == 1:
